f7b26a35cf32f0aa7caa1000d7ab8f5374159532
Saraivinha1703 <carlos.saraiva.neto@gmail.com>
2023-10-26 16:23:32 +0100

    package

--- packages/Shared/index.ts
+++ packages/Shared/index.ts
@@ -1,3 +1,62 @@
-export const Test = {
-  prop: 'testing',
+type ColorLevelProps = {
+  background: string;
+  text: string;
+};
+
+type ThemeColorLevel = {
+  primary: ColorLevelProps;
+  secondary: ColorLevelProps;
+};
+
+type ThemeCollection = {
+  light: ThemeColorLevel;
+  dark: ThemeColorLevel;
+  halloween: ThemeColorLevel;
+  xmas: ThemeColorLevel;
+};
+
+export const Theme: ThemeCollection = {
+  light: {
+    primary: {
+      background: '',
+      text: '',
+    },
+    secondary: {
+      background: '',
+      text: '',
+    },
+  },
+
+  dark: {
+    primary: {
+      background: '',
+      text: '',
+    },
+    secondary: {
+      background: '',
+      text: '',
+    },
+  },
+
+  halloween: {
+    primary: {
+      background: '',
+      text: '',
+    },
+    secondary: {
+      background: '',
+      text: '',
+    },
+  },
+
+  xmas: {
+    primary: {
+      background: '',
+      text: '',
+    },
+    secondary: {
+      background: '',
+      text: '',
+    },
+  },
 };
