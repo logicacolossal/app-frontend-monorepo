11be658de0eb421c5bb866ba5a0f275f85cfe859
Saraivinha1703 <carlos.saraiva.neto@gmail.com>
2023-10-27 13:46:53 +0100

    taliwind mobile config

--- packages/Mobile/tailwind.config.js
+++ packages/Mobile/tailwind.config.js
@@ -2,13 +2,13 @@
 module.exports = {
   presets: [require('@expo-monorepo/shared/tailwind.config')],
   content: [
-    './index,{js,jsx,ts,tsx}',
-    './App,{js,jsx,ts,tsx}',
+    './index.{js,jsx,ts,tsx}',
+    './App.{js,jsx,ts,tsx}',
     './src/**/*.{js,jsx,ts,tsx}',
     '../Shared/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('nativewind/tailwind/native')],
 };
