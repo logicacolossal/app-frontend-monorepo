d347b96d20ecd8aac897f98a1550848c63111ae3
Saraivinha1703 <carlos.saraiva.neto@gmail.com>
2023-11-02 09:39:03 +0000

    testing

--- packages/Mobile/App.tsx
+++ packages/Mobile/App.tsx
@@ -3,8 +3,10 @@ import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View className='flex-1 items-center justify-center bg-zinc-200'>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-pastel-green-500 dark:text-white">
+        Open up App.js to start working on your app!
+      </Text>
 
       <StatusBar />
     </View>
